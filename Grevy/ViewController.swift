7b84db0801d6510c8133ebc3a8f17de350605074
Judy <paoc@umich.edu>
2019-07-18 19:38:31 -0400

    Grevy 5 - setting up environment

---
+++ Grevy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grevy
+//
+//  Created by Judy Pao on 7/18/19.
+//  Copyright © 2019 Judy Pao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
